506a6534572327e4f7332a542393edaaaaddaf8a
nuno1414 <n_eskim@naver.com>
2024-03-04 18:37:24 +0900

    Coding Recipe Ajax Example Project Complete

---
+++ src/main/java/com/codingrecipe/ajaxEx/dto/AjaxDTO.java
@@ -0,0 +1,14 @@
+package com.codingrecipe.ajaxEx.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class AjaxDTO {
+
+    private String param1;
+    private String param2;
+
+}
